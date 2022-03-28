62c646dfc98d712da187b617671d41892480ea8a
ejgenc@gmail.com <ejgenc@gmail.com>
2022-03-28 14:56:27 +0200

    Testing the style-check ci/cd workflow.

---
+++ tests/trigger.py
@@ -0,0 +1,7 @@
+# this will trigger the style-check ci/cd workflow on merge
+# the workflow will fail because it does not comply with our style guide
+import pandas as pd
+import os
+
+fail = [1,2,  5,
+6]
